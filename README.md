16a5a71da1bcbaafd958374921183f643c624af0
umedith <umedith03@gmail.com>
2020-09-27 07:51:03 +0200

    add a readme file

---
+++ README.md
@@ -0,0 +1,28 @@
+# Akan Name Teller
+
+#### Description
+Have yoou ever wondered the day on whih you were born? You are in the right hands now! this is a form that will require you to input you birth date,month and year in addition to your gender, then after perfomimg some functions it will show you the day on which you were born! Have fun!!!
+
+## Setup/Installation Requirements
+* First of all to acces this form you must have a github page
+* Then you should clone it to access it on your laptop
+* Then you can use it as it is or do some changees!
+
+## Known Bugs
+To be cautious you should only enter one number in the input of the year as provided to avoid complications as long as we are working on it!
+
+## Technologies Used
+This form was built under the use of 
+* HTML to display the content, 
+* CSS to make it more presentable and 
+* Javascript to perform some functions and interact with the user.
+
+## Support and contact details
+In any case of complications,comments or contributions reach out to me at :
+* email: umedith03@gmail.com
+* github: umedith
+* contact: +250781703331
+* twitter: Umedith@twitter
+
+### License
+Copyright (c) {2020} 
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-var getAkanName = function(bithDate,gender) {
-    return  (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
-};
+function giveAkanName(){
+
+var day = document.getElementById("day").value;
+var month = document.getElementById("month").value;
+var thousand = document.getElementById("thous").value;
+var hundred = document.getElementById("hund").value;
+var tens = document.getElementById("ten").value;
+var ones= document.getElementById("one").value;
+
+if (day < 1 || day> 31){
+    if (month<1 || month >12){
+        alert("the day and month are invalid");
+    }else{
+        alert("the day is invalid!!");
+    }
+
+}else {
+    if (month<1 || month>12){
+        alert('the month is invalid!!');
+    }
+}
+
+var century= thousand +''+hundred;
+var decade = tens+''+ones;
+
+var birthDate = (((century/4) -2*century-1) + ((5*decade/4) ) + ((26*(month+1)/10)) + day ) % 7;
+document.write(birthDate);
+
+}
+
+giveAkanName();
+
+
+
+
+// var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+// var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+// var getAkanName = function(bithDate,gender) {
+//     return  (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
+// };
